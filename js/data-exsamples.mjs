6bca9b3f301e1cb80466fba8a7262a38ae9d751b
Leandro <leandro83g@icloud.com>
2021-02-03 01:54:46 +0900

    chenging file extentions, and sepalating db file

---
+++ js/data-exsamples.mjs
@@ -0,0 +1,175 @@
+//=================================================
+// data exsamples for testing
+//=================================================
+export const battery =[
+    {
+        "batteryName": "WKL-78",
+        "capacityAh": 2.3,
+        "voltage": 14.4,
+        "maxDraw": 3.2,
+        "endVoltage": 10,
+    },
+    {
+        "batteryName": "WKL-140",
+        "capacityAh": 4.5,
+        "voltage": 14.4,
+        "maxDraw": 9.2,
+        "endVoltage": 5,
+    },
+    {
+        "batteryName": "Wmacro-78",
+        "capacityAh": 2.5,
+        "voltage": 14.5,
+        "maxDraw": 10,
+        "endVoltage": 5,
+    },
+    {
+        "batteryName": "Wmacro-140",
+        "capacityAh": 3.6,
+        "voltage": 14.4,
+        "maxDraw": 14,
+        "endVoltage": 5,
+    },
+    {
+        "batteryName": "IOP-E78",
+        "capacityAh": 6.6,
+        "voltage": 14.4,
+        "maxDraw": 10.5,
+        "endVoltage": 8,
+    },
+    {
+        "batteryName": "IOP-E140",
+        "capacityAh": 9.9,
+        "voltage": 14.4,
+        "maxDraw": 14,
+        "endVoltage": 10,
+    },
+    {
+        "batteryName": "IOP-E188",
+        "capacityAh": 13.2,
+        "voltage": 14.4,
+        "maxDraw": 14,
+        "endVoltage": 11,
+    },
+    {
+        "batteryName": "RYN-C65",
+        "capacityAh": 4.9,
+        "voltage": 14.8,
+        "maxDraw": 4.9,
+        "endVoltage": 11,
+    },
+    {
+        "batteryName": "RYN-C85",
+        "capacityAh": 6.3,
+        "voltage": 14.4,
+        "maxDraw": 6.3,
+        "endVoltage": 12,
+    },
+    {
+        "batteryName": "RYN-C140",
+        "capacityAh": 9.8,
+        "voltage": 14.8,
+        "maxDraw": 10,
+        "endVoltage": 12,
+    },
+    {
+        "batteryName": "RYN-C290",
+        "capacityAh": 19.8,
+        "voltage": 14.4,
+        "maxDraw": 14,
+        "endVoltage": 12,
+    }
+];
+
+export const camera =[
+    {
+        "brand_id": 1,
+        "model": "ABC 3000M",
+        "powerConsumptionWh": 35.5,
+    },
+    {
+        "brand_id": 1,
+        "model": "ABC 5000M",
+        "powerConsumptionWh": 37.2,
+    },
+    {
+        "brand_id": 1,
+        "model": "ABC 7000M",
+        "powerConsumptionWh": 39.7,
+    },
+    {
+        "brand_id": 1,
+        "model": "ABC 9000M",
+        "powerConsumptionWh": 10.9,
+    },
+    {
+        "brand_id": 1,
+        "model": "ABC 9900M",
+        "powerConsumptionWh": 15.7,
+    },
+    {
+        "brand_id": 2,
+        "model": "UIK 110C",
+        "powerConsumptionWh": 62.3,
+    },
+    {
+        "brand_id": 2,
+        "model": "UIK 210C",
+        "powerConsumptionWh": 64.3,
+    },
+    {
+        "brand_id": 2,
+        "model": "UIK 230C",
+        "powerConsumptionWh": 26.3,
+    },
+    {
+        "brand_id": 2,
+        "model": "UIK 250C",
+        "powerConsumptionWh": 15.3,
+    },
+    {
+        "brand_id": 2,
+        "model": "UIK 270C",
+        "powerConsumptionWh": 20.3,
+    },
+    {
+        "brand_id": 3,
+        "model": "CEV 1100P",
+        "powerConsumptionWh": 22,
+    },
+    {
+        "brand_id": 3,
+        "model": "CEV 1300P",
+        "powerConsumptionWh": 23,
+    },
+    {
+        "brand_id": 3,
+        "model": "CEV 1500P",
+        "powerConsumptionWh": 24,
+    },
+    {
+        "brand_id": 3,
+        "model": "CEV 1700P",
+        "powerConsumptionWh": 25,
+    },
+    {
+        "brand_id": 3,
+        "model": "CEV 1900P",
+        "powerConsumptionWh": 26,
+    }
+];
+
+export const brand = [
+    {
+        "camera_ids": { 1: 1, 2: 2, 3: 3, 4: 4, 5: 5 },
+        "name": "Cakon",
+    },
+    {
+        "camera_ids": { 6: 6, 7: 6, 8: 6, 9: 6, 10: 10 },
+        "name": "Go MN",
+    },
+    {
+        "camera_ids": { 11: 11, 12: 12, 13: 13, 14: 14, 15: 15 },
+        "name": "VANY",
+    }
+];
--- js/db.mjs
+++ js/db.mjs
@@ -1,3 +1,7 @@
+import { battery } from './data-exsamples.mjs'
+import { camera  } from './data-exsamples.mjs'
+import { brand   } from './data-exsamples.mjs'
+
 //=================================================
 //DBクラス (DB Class)
 //=================================================
@@ -198,179 +202,7 @@ class Brand extends DB {
     }
 }
 
-//=================================================
-// テスト用のデータ(test data)
-//=================================================
-const battery =[
-    {
-        "batteryName": "WKL-78",
-        "capacityAh": 2.3,
-        "voltage": 14.4,
-        "maxDraw": 3.2,
-        "endVoltage": 10,
-    },
-    {
-        "batteryName": "WKL-140",
-        "capacityAh": 4.5,
-        "voltage": 14.4,
-        "maxDraw": 9.2,
-        "endVoltage": 5,
-    },
-    {
-        "batteryName": "Wmacro-78",
-        "capacityAh": 2.5,
-        "voltage": 14.5,
-        "maxDraw": 10,
-        "endVoltage": 5,
-    },
-    {
-        "batteryName": "Wmacro-140",
-        "capacityAh": 3.6,
-        "voltage": 14.4,
-        "maxDraw": 14,
-        "endVoltage": 5,
-    },
-    {
-        "batteryName": "IOP-E78",
-        "capacityAh": 6.6,
-        "voltage": 14.4,
-        "maxDraw": 10.5,
-        "endVoltage": 8,
-    },
-    {
-        "batteryName": "IOP-E140",
-        "capacityAh": 9.9,
-        "voltage": 14.4,
-        "maxDraw": 14,
-        "endVoltage": 10,
-    },
-    {
-        "batteryName": "IOP-E188",
-        "capacityAh": 13.2,
-        "voltage": 14.4,
-        "maxDraw": 14,
-        "endVoltage": 11,
-    },
-    {
-        "batteryName": "RYN-C65",
-        "capacityAh": 4.9,
-        "voltage": 14.8,
-        "maxDraw": 4.9,
-        "endVoltage": 11,
-    },
-    {
-        "batteryName": "RYN-C85",
-        "capacityAh": 6.3,
-        "voltage": 14.4,
-        "maxDraw": 6.3,
-        "endVoltage": 12,
-    },
-    {
-        "batteryName": "RYN-C140",
-        "capacityAh": 9.8,
-        "voltage": 14.8,
-        "maxDraw": 10,
-        "endVoltage": 12,
-    },
-    {
-        "batteryName": "RYN-C290",
-        "capacityAh": 19.8,
-        "voltage": 14.4,
-        "maxDraw": 14,
-        "endVoltage": 12,
-    }
-];
-const camera =[
-    {
-        "brand_id": 1,
-        "model": "ABC 3000M",
-        "powerConsumptionWh": 35.5,
-    },
-    {
-        "brand_id": 1,
-        "model": "ABC 5000M",
-        "powerConsumptionWh": 37.2,
-    },
-    {
-        "brand_id": 1,
-        "model": "ABC 7000M",
-        "powerConsumptionWh": 39.7,
-    },
-    {
-        "brand_id": 1,
-        "model": "ABC 9000M",
-        "powerConsumptionWh": 10.9,
-    },
-    {
-        "brand_id": 1,
-        "model": "ABC 9900M",
-        "powerConsumptionWh": 15.7,
-    },
-    {
-        "brand_id": 2,
-        "model": "UIK 110C",
-        "powerConsumptionWh": 62.3,
-    },
-    {
-        "brand_id": 2,
-        "model": "UIK 210C",
-        "powerConsumptionWh": 64.3,
-    },
-    {
-        "brand_id": 2,
-        "model": "UIK 230C",
-        "powerConsumptionWh": 26.3,
-    },
-    {
-        "brand_id": 2,
-        "model": "UIK 250C",
-        "powerConsumptionWh": 15.3,
-    },
-    {
-        "brand_id": 2,
-        "model": "UIK 270C",
-        "powerConsumptionWh": 20.3,
-    },
-    {
-        "brand_id": 3,
-        "model": "CEV 1100P",
-        "powerConsumptionWh": 22,
-    },
-    {
-        "brand_id": 3,
-        "model": "CEV 1300P",
-        "powerConsumptionWh": 23,
-    },
-    {
-        "brand_id": 3,
-        "model": "CEV 1500P",
-        "powerConsumptionWh": 24,
-    },
-    {
-        "brand_id": 3,
-        "model": "CEV 1700P",
-        "powerConsumptionWh": 25,
-    },
-    {
-        "brand_id": 3,
-        "model": "CEV 1900P",
-        "powerConsumptionWh": 26,
-    }
-];
-const brand = [
-    {
-        "camera_ids": { 1: 1, 2: 2, 3: 3, 4: 4, 5: 5 },
-        "name": "Cakon",
-    },
-    {
-        "camera_ids": { 6: 6, 7: 6, 8: 6, 9: 6, 10: 10 },
-        "name": "Go MN",
-    },
-    {
-        "camera_ids": { 11: 11, 12: 12, 13: 13, 14: 14, 15: 15 },
-        "name": "VANY",
-    }
-];
+
 //==============================================
 //DBの初期化 (initilize DB by test data)
 //==============================================
@@ -437,34 +269,36 @@ console.log(DB.showDB())
 // console.log(DB.table["Camera"][1]["id"]) -column
 //===================================================
 
-// let z = {
-//     1:{"v_id":1,},
-//     2:{"v_id":1,},
-//     3:{"v_id":2,},
-//     4:{"v_id":1,},
-// }
+let z = {
+    1:{"v_id":1,},
+    2:{"v_id":1,},
+    3:{"v_id":2,},
+    4:{"v_id":1,},
+}
 
-// let v = {
-//     "id":1,
-//     "zs":test
-// }
+let v = {
+    "id":1,
+    "zs":test
+}
 
-// let vStr = "v"
-// function test(string){
-//     let newObj = {}
+let vStr = "v"
+function test(string){
+    let newObj = {}
 
-//     Object.keys(z).forEach(record=> {
+    Object.keys(z).forEach(record=> {
 
-//         if(z[record][`${string}_id`] == v.id){
-//             newObj[record] = z[record]
-//         }
+        if(z[record][`${string}_id`] == v.id){
+            newObj[record] = z[record]
+        }
 
-//     })
+    })
 
-//     return newObj;
-// }
+    return newObj;
+}
 
-// console.log(v.zs(vStr))
+console.log(v.zs(vStr))
 
-// z[5] = { "v_id":1}
-// console.log(v.zs(vStr))
+z[5] = { "v_id":1}
+z[6] = { "v_id":2}
+
+console.log(v.zs(vStr))
